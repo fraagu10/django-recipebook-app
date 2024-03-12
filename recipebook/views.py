73db66bddf11e82e5792e3e06c3df5d906ce2ae9
fraagu10 <aguilarfranco1092@gmail.com>
2024-03-12 18:07:32 -0500

    added details view for recipes

--- recipebook/views.py
+++ recipebook/views.py
@@ -1,8 +1,11 @@
-from django.shortcuts import render
-from django.utils import timezone
+from django.shortcuts import render, get_object_or_404
 from .models import Recipe
 
 # Create your views here.
 def recipe_list(request):
     recipes = Recipe.objects.all().order_by('created_date')
-    return render(request, 'recipebook/recipe_list.html', {'recipes': recipes})
+    return render(request, 'recipe_list/recipe_list.html', {'recipes': recipes})
+
+def recipe_detail(request, pk):
+    recipe = get_object_or_404(Recipe, pk=pk)
+    return render(request, 'recipe_list/recipe_detail.html', {'recipe': recipe})
